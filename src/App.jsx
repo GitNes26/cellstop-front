46ed2cab5acb4819fba095e88badc6ef8e59c2e6
GitNes26 <nestorpuentesin@gmail.com>
2025-11-08 12:09:08 -0600

    quite espacio en balco del app

--- src/App.jsx
+++ src/App.jsx
@@ -92,7 +92,7 @@ export default function App({}) {
                   <Loading open={isLoading} animation="bounce" />
                   <RouterProvider router={router} fallbackElement={<LinearLoading />} />
                </SnackbarProvider>
-            </LocalizationProvider>{" "}
+            </LocalizationProvider>
          </QueryClientProvider>
       </>
    );
