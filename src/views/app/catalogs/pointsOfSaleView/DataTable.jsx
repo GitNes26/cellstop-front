9b1da980311a17b53a8f87b500e990a04cabcc73
Nestor <nestorpuentesin@gmail.com>
2026-02-19 10:32:44 -0600

    se acoplo dataTablesNet en POS pero aun le falta

--- src/views/app/catalogs/pointsOfSaleView/DataTable.jsx
+++ src/views/app/catalogs/pointsOfSaleView/DataTable.jsx
@@ -1,4 +1,5 @@
 import React, { useCallback, useEffect } from "react";
+import ReactDOMServer from "react-dom/server";
 import { Typography, Button, ButtonGroup, Tooltip, Avatar } from "@mui/material";
 
 import Toast from "../../../../utils/Toast";
@@ -32,167 +33,271 @@ const PointOfSaleDT = () => {
    } = usePointOfSaleContext();
    const mySwal = withReactContent(Swal);
 
-   //#region COLUMNAS
+   // //#region COLUMNAS mui
+   // const fontSizeTable = { text: "sm", subtext: "xs" };
+   // const globalFilterFields = ["pointOfSale", "description", "active", "created_at"];
+
+   // // #region BodysTemplate
+   // const ImgBodyTemplate = (obj) => (
+   //    <>{obj.img == null || obj.img === "" ? <Avatar {...stringAvatar(obj.name)} /> : <Avatar src={`${env.API_URL_IMG}/${obj.img}`} />}</>
+   // );
+
+   // const PointOfSaleBodyTemplate = (obj) => (
+   //    <Typography textAlign={"center"} size={fontSizeTable.text}>
+   //       {obj.name}
+   //    </Typography>
+   // );
+   // const ContactBodyTemplate = (obj) => (
+   //    <>
+   //       <Typography textAlign={"center"} size={fontSizeTable.text}>
+   //          {obj.contact_name}
+   //       </Typography>
+   //       <Typography textAlign={"center"} size={fontSizeTable.subtext} className="flex items-center justify-center italic">
+   //          <PhoneAndroidRounded style={{ color: "" }} fontSize={"medium"} className="mr-2" />
+   //          <a href={`tel:${obj.contact_phone}`} className="text-blue-600 hover:underline transition-all">
+   //             {formatPhone(obj.contact_phone)}
+   //          </a>
+   //       </Typography>
+   //    </>
+   // );
+   // const AddressBodyTemplate = (obj) => (
+   //    <>
+   //       <Typography
+   //          textAlign={"center"}
+   //          size={fontSizeTable.text}
+   //          component={"a"}
+   //          href={obj.ubication ?? "#"}
+   //          target="_blank"
+   //          className="text-blue-800 hover:underline transition-all"
+   //       >
+   //          <MapRounded style={{ color: "" }} fontSize={"medium"} className="mr-2" /> {obj.address}
+   //       </Typography>
+   //    </>
+   // );
+   // const SellerBodyTemplate = (obj) => (
+   //    <Typography textAlign={"center"} size={fontSizeTable.text}>
+   //       {obj.seller.username}
+   //    </Typography>
+   // );
+
+   // const ActiveBodyTemplate = (obj) => (
+   //    <Typography textAlign={"center"} className="flex justify-center">
+   //       {obj.active ? <CheckCircleRounded style={{ color: "green" }} fontSize={"medium"} /> : <CancelRounded style={{ color: "red" }} fontSize={"medium"} />}
+   //    </Typography>
+   // );
+   // const CreatedAtBodyTemplate = (obj) => (
+   //    <Typography textAlign={"center"} size={fontSizeTable.text}>
+   //       {formatDatetime(obj.created_at, true)}
+   //    </Typography>
+   // );
+   // // #endregion BodysTemplate
+
+   // const columns = [
+   //    {
+   //       field: "img",
+   //       headerName: "Img",
+   //       description: "",
+   //       // width: 90,
+   //       sortable: false,
+   //       functionEdit: null,
+   //       renderCell: (params) => <ImgBodyTemplate {...params.row} key={`img-${params.row.id}`} />,
+   //       filter: false,
+   //       filterField: null
+   //    },
+   //    {
+   //       field: "name",
+   //       headerName: "Puesto de trabajo",
+   //       description: "",
+   //       // width: 90,
+   //       sortable: true,
+   //       functionEdit: null,
+   //       renderCell: (params) => {
+   //          const { key, ...obj } = params.row;
+   //          return <PointOfSaleBodyTemplate {...obj} key={`name-${params.row.id}`} />;
+   //       },
+   //       filter: true,
+   //       filterField: null
+   //    },
+   //    {
+   //       field: "contact_name",
+   //       headerName: "Contacto",
+   //       description: "",
+   //       // width: 90,
+   //       sortable: true,
+   //       functionEdit: null,
+   //       renderCell: (params) => {
+   //          const { key, ...obj } = params.row;
+   //          return <ContactBodyTemplate {...obj} key={`contact_name-${params.row.id}`} />;
+   //       },
+   //       filter: true,
+   //       filterField: null
+   //    },
+   //    {
+   //       field: "address",
+   //       headerName: "Dirección",
+   //       description: "",
+   //       // width: 90,
+   //       sortable: true,
+   //       functionEdit: null,
+   //       renderCell: (params) => {
+   //          const { key, ...obj } = params.row;
+   //          return <AddressBodyTemplate {...obj} key={`address-${params.row.id}`} />;
+   //       },
+   //       filter: true,
+   //       filterField: null
+   //    },
+   //    {
+   //       field: "seller",
+   //       headerName: "Vendedor",
+   //       description: "",
+   //       // width: 90,
+   //       sortable: true,
+   //       functionEdit: null,
+   //       renderCell: (params) => {
+   //          const { key, ...obj } = params.row;
+   //          return <SellerBodyTemplate {...obj} key={`seller-${params.row.id}`} />;
+   //       },
+   //       filter: true,
+   //       filterField: null
+   //    }
+   // ];
+   // auth.role_id === ROLE_SUPER_ADMIN &&
+   //    columns.push(
+   //       {
+   //          field: "active",
+   //          headerName: "Activo",
+   //          description: "",
+   //          // width: 90,
+   //          sortable: true,
+   //          functionEdit: null,
+   //          renderCell: (params) => {
+   //             const { key, ...obj } = params.row;
+   //             return <ActiveBodyTemplate {...obj} key={`active-${params.row.id}`} />;
+   //          },
+   //          filter: false,
+   //          filterField: null
+   //       },
+   //       {
+   //          field: "created_at",
+   //          headerName: "Fecha de alta",
+   //          description: "",
+   //          // width: 90,
+   //          sortable: true,
+   //          functionEdit: null,
+   //          renderCell: (params) => {
+   //             const { key, ...obj } = params.row;
+   //             return <CreatedAtBodyTemplate {...obj} key={`created_at-${params.row.id}`} />;
+   //          },
+   //          filter: false,
+   //          filterField: null
+   //       }
+   //    );
+   // //#endregion COLUMNAS
+
+   //#region OPCION DE DataTables.Net
    const fontSizeTable = { text: "sm", subtext: "xs" };
-   const globalFilterFields = ["pointOfSale", "description", "active", "created_at"];
 
-   // #region BodysTemplate
-   const ImgBodyTemplate = (obj) => (
-      <>{obj.img == null || obj.img === "" ? <Avatar {...stringAvatar(obj.name)} /> : <Avatar src={`${env.API_URL_IMG}/${obj.img}`} />}</>
-   );
+   const ImgBodyTemplate = (obj) =>
+      obj.img == null || obj.img === ""
+         ? ReactDOMServer.renderToString(<Avatar {...stringAvatar(obj.name)} />)
+         : ReactDOMServer.renderToString(<Avatar src={`${env.API_URL_IMG}/${obj.img}`} />);
 
-   const PointOfSaleBodyTemplate = (obj) => (
-      <Typography textAlign={"center"} size={fontSizeTable.text}>
-         {obj.name}
-      </Typography>
-   );
-   const ContactBodyTemplate = (obj) => (
-      <>
-         <Typography textAlign={"center"} size={fontSizeTable.text}>
-            {obj.contact_name}
+   const ContactBodyTemplate = (obj) =>
+      ReactDOMServer.renderToString(
+         <>
+            {" "}
+            <Typography textAlign="center" size={fontSizeTable.text}>
+               {" "}
+               {obj.contact_name}{" "}
+            </Typography>{" "}
+            <Typography textAlign="center" size={fontSizeTable.subtext} className="flex items-center justify-center italic">
+               {" "}
+               <PhoneAndroidRounded fontSize="medium" className="mr-2" />{" "}
+               <a href={`tel:${obj.contact_phone}`} className="text-blue-600 hover:underline transition-all">
+                  {" "}
+                  {formatPhone(obj.contact_phone)}{" "}
+               </a>{" "}
+            </Typography>{" "}
+         </>
+      );
+   const PointOfSaleBodyTemplate = (obj) =>
+      ReactDOMServer.renderToString(
+         <Typography textAlign="center" size={fontSizeTable.text}>
+            {obj.name}
          </Typography>
-         <Typography textAlign={"center"} size={fontSizeTable.subtext} className="flex items-center justify-center italic">
-            <PhoneAndroidRounded style={{ color: "" }} fontSize={"medium"} className="mr-2" />
-            <a href={`tel:${obj.contact_phone}`} className="text-blue-600 hover:underline transition-all">
-               {formatPhone(obj.contact_phone)}
-            </a>
-         </Typography>
-      </>
-   );
-   const AddressBodyTemplate = (obj) => (
-      <>
+      );
+   const AddressBodyTemplate = (obj) =>
+      ReactDOMServer.renderToString(
          <Typography
-            textAlign={"center"}
+            textAlign="center"
             size={fontSizeTable.text}
-            component={"a"}
+            component="a"
             href={obj.ubication ?? "#"}
             target="_blank"
             className="text-blue-800 hover:underline transition-all"
          >
-            <MapRounded style={{ color: "" }} fontSize={"medium"} className="mr-2" /> {obj.address}
+            {" "}
+            <MapRounded fontSize="medium" className="mr-2" /> {obj.address}{" "}
          </Typography>
-      </>
-   );
-   const SellerBodyTemplate = (obj) => (
-      <Typography textAlign={"center"} size={fontSizeTable.text}>
-         {obj.seller.username}
-      </Typography>
-   );
+      );
 
-   const ActiveBodyTemplate = (obj) => (
-      <Typography textAlign={"center"} className="flex justify-center">
-         {obj.active ? <CheckCircleRounded style={{ color: "green" }} fontSize={"medium"} /> : <CancelRounded style={{ color: "red" }} fontSize={"medium"} />}
-      </Typography>
-   );
-   const CreatedAtBodyTemplate = (obj) => (
-      <Typography textAlign={"center"} size={fontSizeTable.text}>
-         {formatDatetime(obj.created_at, true)}
-      </Typography>
-   );
-   // #endregion BodysTemplate
+   const SellerBodyTemplate = (obj) =>
+      ReactDOMServer.renderToString(
+         <Typography textAlign="center" size={fontSizeTable.text}>
+            {obj.seller.username}
+         </Typography>
+      );
+
+   const ActiveBodyTemplate = (obj) =>
+      ReactDOMServer.renderToString(
+         <Typography textAlign={"center"} className="flex justify-center">
+            {obj.active ? <CheckCircleRounded style={{ color: "green" }} fontSize={"medium"} /> : <CancelRounded style={{ color: "red" }} fontSize={"medium"} />}
+         </Typography>
+      );
+
+   const CreatedAtBodyTemplate = (obj) =>
+      ReactDOMServer.renderToString(
+         <Typography textAlign={"center"} size={fontSizeTable.text}>
+            {formatDatetime(obj.created_at, true)}
+         </Typography>
+      );
 
    const columns = [
+      { title: "Img", data: "img", orderable: false, render: (data, type, row) => ImgBodyTemplate(row) },
       {
-         field: "img",
-         headerName: "Img",
-         description: "",
-         // width: 90,
-         sortable: false,
-         functionEdit: null,
-         renderCell: (params) => <ImgBodyTemplate {...params.row} key={`img-${params.row.id}`} />,
-         filter: false,
-         filterField: null
+         title: "Punto de venta",
+         data: "pos_name",
+         orderable: true,
+         render: (data, type, row) => PointOfSaleBodyTemplate(row)
+      },
+      { title: "Contacto", data: "contact_name", orderable: true, render: (data, type, row) => ContactBodyTemplate(row) },
+      {
+         title: "Dirección",
+         data: "address",
+         orderable: true,
+         render: (data, type, row) => AddressBodyTemplate(row)
       },
       {
-         field: "name",
-         headerName: "Puesto de trabajo",
-         description: "",
-         // width: 90,
-         sortable: true,
-         functionEdit: null,
-         renderCell: (params) => {
-            const { key, ...obj } = params.row;
-            return <PointOfSaleBodyTemplate {...obj} key={`name-${params.row.id}`} />;
-         },
-         filter: true,
-         filterField: null
+         title: "Vendedor",
+         data: "seller.username",
+         orderable: true,
+         render: (data, type, row) => SellerBodyTemplate(row)
       },
       {
-         field: "contact_name",
-         headerName: "Contacto",
-         description: "",
-         // width: 90,
-         sortable: true,
-         functionEdit: null,
-         renderCell: (params) => {
-            const { key, ...obj } = params.row;
-            return <ContactBodyTemplate {...obj} key={`contact_name-${params.row.id}`} />;
-         },
-         filter: true,
-         filterField: null
+         title: "Activo",
+         data: "active",
+         orderable: true,
+         render: (data, type, row) => ActiveBodyTemplate(row)
       },
       {
-         field: "address",
-         headerName: "Dirección",
-         description: "",
-         // width: 90,
-         sortable: true,
-         functionEdit: null,
-         renderCell: (params) => {
-            const { key, ...obj } = params.row;
-            return <AddressBodyTemplate {...obj} key={`address-${params.row.id}`} />;
-         },
-         filter: true,
-         filterField: null
-      },
-      {
-         field: "seller",
-         headerName: "Vendedor",
-         description: "",
-         // width: 90,
-         sortable: true,
-         functionEdit: null,
-         renderCell: (params) => {
-            const { key, ...obj } = params.row;
-            return <SellerBodyTemplate {...obj} key={`seller-${params.row.id}`} />;
-         },
-         filter: true,
-         filterField: null
+         title: "Fecha de alta",
+         data: "created_at",
+         orderable: true,
+         render: (data, type, row) => CreatedAtBodyTemplate(row)
       }
    ];
-   auth.role_id === ROLE_SUPER_ADMIN &&
-      columns.push(
-         {
-            field: "active",
-            headerName: "Activo",
-            description: "",
-            // width: 90,
-            sortable: true,
-            functionEdit: null,
-            renderCell: (params) => {
-               const { key, ...obj } = params.row;
-               return <ActiveBodyTemplate {...obj} key={`active-${params.row.id}`} />;
-            },
-            filter: false,
-            filterField: null
-         },
-         {
-            field: "created_at",
-            headerName: "Fecha de alta",
-            description: "",
-            // width: 90,
-            sortable: true,
-            functionEdit: null,
-            renderCell: (params) => {
-               const { key, ...obj } = params.row;
-               return <CreatedAtBodyTemplate {...obj} key={`created_at-${params.row.id}`} />;
-            },
-            filter: false,
-            filterField: null
-         }
-      );
-   //#endregion COLUMNAS
 
+   //#endregion OPCION DE DataTables.Net
    const handleClickAdd = () => {
       try {
          if (formikRef.current === null) setOpenDialog(true);
@@ -338,11 +443,7 @@ const PointOfSaleDT = () => {
          title="Puestos de trabajo"
          data={data}
          // data={myData}
-         columns={[
-            { title: "ID", data: "id" },
-            { title: "Nombre", data: "name" },
-            { title: "Email", data: "email" }
-         ]}
+         columns={columns ?? []}
          serverSide={false} // o true si viene de Laravel
          ajaxUrl="/api/users"
          onRowSelect={(rows) => console.log("Seleccionados:", rows)}
